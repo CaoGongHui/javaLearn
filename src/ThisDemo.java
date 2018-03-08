4155561dd866b3378d64653646234863acf2d338
CaoGongHui <847781669@qq.com>
2018-03-08 21:20:39 +0800

    add a function compare

--- src/ThisDemo.java
+++ src/ThisDemo.java
@@ -22,15 +22,24 @@ class Person{
     public void speak(){
         System.out.println(name+":"+age);
     }
+    public boolean compare(Person p){
+        if (this.age==p.age)
+            return true;
+        else
+            return  false;
+    }
 
 
 }
 class ThisDemo{
     public static void main(String[] args) {
-        Person p=new Person("Xiao Qiang",30);
+        Person p1=new Person("aa",23);
+        Person p2=new Person("vv",34);
+        p1.compare(p2);
+ /*       Person p=new Person("Xiao Qiang",30);
         p.speak();
         Person p1=new Person("Wang cai");
         p1.speak();
-        Person newPerson2 = new Person();
+        Person newPerson2 = new Person();*/
     }
 }
