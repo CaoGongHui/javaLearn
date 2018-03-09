849e08a2aba006653e306015e3c9329ef8a712cb
CaoGongHui <847781669@qq.com>
2018-03-09 15:03:24 +0800

    解释成员变量与静态变量

--- src/StaticDemo.java
+++ src/StaticDemo.java
@@ -1,13 +1,20 @@
 public class StaticDemo {
+    int num =4;
+
     public static void main(String[] args) {
         Person p=new Person();
         p.name="小强";
         p.show();
+        new StaticDemo().show();
     }
+    public  void show(){
+        System.out.println(num);
+    }
+
 }
 class Person{
-    String name;
-    static String country="cn";
+    String name;//成员变量（实例变量）
+    static String country="cn";//静态变量，类变量
     public void show(){
         System.out.println(country+":"+name);
     }
