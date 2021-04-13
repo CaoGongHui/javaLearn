8aebec23d03638657b7569d8068be1cc918050e2
CaoGongHui <847781669@qq.com>
2021-04-13 16:06:01 +0800

    debug

--- src/Array2Demo.java
+++ src/Array2Demo.java
@@ -3,7 +3,7 @@ public class Array2Demo {
         int [][] arr=new int[3][2];//创建一个二维数组。3个一维数组，每个一维数组里有两个元素
 
         System.out.println(arr);//直接打印二维数组
-        System.out.println(arr[0]);//打印二维数组中的教表为零的一维数组
+        System.out.println(arr[1]);//打印二维数组中的教表为零的一维数组
         System.out.println(arr[1][1]);
     }
 }
